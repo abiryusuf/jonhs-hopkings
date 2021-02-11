c15d2714949b637c294007a936b3b79f343d5925
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-10 23:19:38 -0500

    evey day javaScriot

--- javaScript/acronym.js
+++ javaScript/acronym.js
@@ -2,20 +2,23 @@
 
 var name = "Abir Yusuf";
 
-const res = name.split("").reverse().join("");
+const res = name.split(" ");
 
 console.log(res)
 
 function acronym(str){
-    var arr = str.split("");
+    var arr = str.split(" ");
 
     var result = "";
     for (let i = 0; i < arr.length; i++) {
         const element = arr[i];
-        
+        result += element[0].toUpperCase();
 
         //console.log(element)
         
     }
+    return result;
 }
-acronym("abir");
+var info = "abir yusuf mim";
+var res1 = acronym(info);
+console.log(res1);
