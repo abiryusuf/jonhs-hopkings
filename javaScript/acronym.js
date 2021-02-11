fdb580c60db5304b6febc4a272de84f8e5502404
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-10 22:59:27 -0500

    trytofix

---
+++ javaScript/acronym.js
@@ -0,0 +1,21 @@
+
+
+var name = "Abir Yusuf";
+
+const res = name.split("").reverse().join("");
+
+console.log(res)
+
+function acronym(str){
+    var arr = str.split("");
+
+    var result = "";
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        
+
+        //console.log(element)
+        
+    }
+}
+acronym("abir");
