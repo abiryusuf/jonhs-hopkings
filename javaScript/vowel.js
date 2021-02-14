1bc50c25e200211a5c5e8980d5a921c2fc9d4e4d
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-13 19:36:55 -0500

    solving

--- javaScript/vowel.js
+++ javaScript/vowel.js
@@ -13,17 +13,35 @@ var vow = checkVowel("yusuf")
 console.log(vow)
 
 function countVowel(word){
-    var arr = word.split("")
+    var arr = word.split("");
     var res = 0;
 
     for (let i = 0; i < arr.length; i++) {
-        const element = arr[i].toUpperCase();
-        if (element === "A" || element === "E" || element ==="I" || element ==="0" || element ==="U") {
-            res +=1;
-        }
-        
+        const element = arr[i];
+        // if (element === "A" || element === "E" || element ==="I" || element ==="0" || element ==="U") {
+        //     res +=1;
+        // }
+        //element ++;
+        return element
+        //console.log(element)
+        //res = element + 1
     }
-    return res
+    //return res;
 }
 var y = countVowel("abir YUSUF");
 console.log(y);
+
+//name = abir yusuf
+
+var nameInfo = "abir yusuf";
+//console.log(nameInfo.split(" "))
+
+function ac(str){
+    var arr = str.split(" ");
+    var res = "";
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        res += element[0].toUpperCase()
+    }
+    return res;
+}
