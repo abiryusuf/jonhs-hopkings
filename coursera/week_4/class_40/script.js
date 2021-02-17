66a19a49b5feaf02771eadefe24616a3a325d8e4
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-17 12:32:26 -0500

    function

---
+++ coursera/week_4/class_40/script.js
@@ -0,0 +1,17 @@
+var name = "abir yusuf";
+
+//console.log(name);
+
+function myFunction(){
+    console.log(name);
+    local();
+}
+myFunction();
+
+
+function local(){
+    var carName = "Honda"
+    console.log(carName);
+}
+
+local();
