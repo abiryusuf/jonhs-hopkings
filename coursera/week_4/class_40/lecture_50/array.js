9a00d860dea4c36edcc82bd962ab3c31a965a89d
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-20 15:29:40 -0500

    array advance

--- coursera/week_4/class_40/lecture_50/array.js
+++ coursera/week_4/class_40/lecture_50/array.js
@@ -1,15 +1,42 @@
-var array = new Array();
+// var array = new Array();
 
-array[0] = "Abir";
-array[1] = 2;
-array[2] = function(name){
-    console.log("Hello " + name);
+// array[0] = "Abir";
+// array[1] = 2;
+// array[2] = function(name){
+//     console.log("Hello " + name);
+// }
+
+// array[3] = {
+//     course: "HTML, CSS and JS"
+// };
+
+// console.log(array);
+
+// array[2](array[0]);
+
+var name = ["Abir", "Yusuf", "Mim"];
+//name = "Madiha";
+for (let i = 0; i < name.length; i++) {
+    const element = name[i];
+    console.log(element);
+    
 }
 
-array[3] = {
-    course: "HTML, CSS and JS"
-};
+name [4]= "Madiha";
 
-console.log(array);
+for(var x in name){
+    console.log("Hello", name[x])
+}
 
-array[2](array[0]);
+var counter = 0;
+
+var myArray = ["Abir", 4, {
+    handle: 
+    "abir yusuf"
+}];
+
+for (let i = 0; i < myArray.length; i++) {
+    counter++
+    
+}
+console.log(counter);
